b5e0443f552f2e4fb4c66b02009f6ee396bc2765
Anjali Karimpil <anjaliskarimpil@gmail.com>
2024-02-02 18:11:36 +0000

    Installed node

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const textInput = document.getElementById("translate-input")
+import {Configuration, OpenAIApi } from "openai"
+// const textInput = document.getElementById("translate-input")
 
-const buttonEl = document.getElementById("translate-btn")
+// const buttonEl = document.getElementById("translate-btn")
 
-buttonEl.addEventListener("click", translate)
+// buttonEl.addEventListener("click", translate)
 
-function translate(event){
-    event.preventDefault()
+// function translate(event){
+//     event.preventDefault()
 
-    // Check if textarea is empty
-    const inputText = textInput.value.trim()
-    const selectedLanguage = document.querySelector('input[name="selected_language"]:checked');
-    if(inputText.value && selectedLanguage){
-        // 
-        setTranslatedText("Set the value returned by openAI API here")
-    }
+//     // Check if textarea is empty
+//     const inputText = textInput.value.trim()
+//     const selectedLanguage = document.querySelector('input[name="selected_language"]:checked');
+//     if(inputText.value && selectedLanguage){
+//         // 
+//         setTranslatedText("Set the value returned by openAI API here")
+//     }
 
         
-}
+// }
 
-function setTranslatedText(translatedText){
-    const resultP = document.getElementById("translated-p")
-    resultP.innerHTML = translatedText
-}
+// function setTranslatedText(translatedText){
+//     const resultP = document.getElementById("translated-p")
+//     resultP.innerHTML = translatedText
+// }
